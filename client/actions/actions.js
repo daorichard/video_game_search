58fbbb25663210cda3e1fa7e5a81417f5b3cf72c
daorichard <98173508+daorichard@users.noreply.github.com>
2023-06-28 21:18:14 -0400

    fixed styling for splide list

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,13 +1,22 @@
-import { popularGamesUrl } from '../components/api';
+import {
+  popularGamesUrl,
+  newGamesUrl,
+  upcoming_gamesUrl,
+} from '../components/api';
+
 import axios from 'axios';
 //ACTION CREATOR
 export const getGameData = () => async (dispatch) => {
   const popData = await axios.get(popularGamesUrl());
+  const newGamesData = await axios.get(newGamesUrl());
+  const upcomingData = await axios.get(upcoming_gamesUrl());
 
   dispatch({
     type: 'FETCH_GAMES',
     payload: {
       popular: popData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import GenreDetails from '../components/GenreDetails';
+import { useDispatch } from 'react-redux';
+import { getGameData } from '../actions/actions';
 
 function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGameData());
+  });
   return (
     <div>
       <Navbar />
       <GenreDetails />
+      Home
     </div>
   );
 }
